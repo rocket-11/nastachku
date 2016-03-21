c802804b20908d97f34cf03c12a3a54d3c64861b
Olesia <olesiatenkova@gmail.com>
2016-03-21 06:40:02 +0000

    fixture registration

--- app/assets/javascripts/web/users.js
+++ app/assets/javascripts/web/users.js
@@ -14,3 +14,21 @@ jQuery(document).ready(function ($) {
     $("#submit").attr("disabled", !this.checked);
   });
 })
+
+$(window).on("load", function () {
+    blockPermutationInfo();
+});
+$(window).on("resize", function () {
+    blockPermutationInfo();
+});
+
+
+function blockPermutationInfo() {
+    var window_width = $(window).width();
+
+    if (window_width < 620) {
+        $(".registration__form").prepend($(".personal__identity__number").eq(0).detach());
+    } else if (window_width >= 620) {
+        $(".personal__identity__social").before($(".personal__identity__number").eq(0).detach());
+    }
+}
